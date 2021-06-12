b6c96d56abbeb830ecaab7ad58dcc77b238b1b25
Cade Richard <cade@caderichard.com>
2021-06-12 07:51:55 -0400

    WOOL plus fixed dye textures

---
+++ src/main/java/net/mcreator/cowpewterworldofcolor/block/LightestCyanWoolBlock.java
@@ -0,0 +1,57 @@
+
+package net.mcreator.cowpewterworldofcolor.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.cowpewterworldofcolor.CowpewterWocModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CowpewterWocModElements.ModElement.Tag
+public class LightestCyanWoolBlock extends CowpewterWocModElements.ModElement {
+	@ObjectHolder("cowpewter_woc:lightest_cyan_wool")
+	public static final Block block = null;
+	public LightestCyanWoolBlock(CowpewterWocModElements instance) {
+		super(instance, 615);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOL).sound(SoundType.CLOTH).hardnessAndResistance(0.8f, 0.8f).setLightLevel(s -> 0));
+			setRegistryName("lightest_cyan_wool");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 30;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
